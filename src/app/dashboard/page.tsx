de5799cf79b83f5470b1d75f0dae1a8fc1147cf7
Markdiocre <mark.manuel.business@gmail.com>
2025-03-31 13:49:01 +0800

    Creation of main features

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div >
+        <h1>Ano meron kayo?</h1>
+
+            </div>
+        </div>
+
+    );
+}
